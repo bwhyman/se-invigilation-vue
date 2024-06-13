ac8537d50600e727d6cdcf6d371d64ff8a2b1815
BO <bwang0629@gmail.com>
2024-06-13 16:49:07 +0800

    update

--- src/views/main/college/functions/InviDetailsView.vue
+++ src/views/main/college/functions/InviDetailsView.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
+import { createElLoading } from '@/components/loading'
 import { listCollegeInviDetailsService, listCollegeCountsService } from '@/services/CollegeService'
 import type { InviDetail } from '@/types'
 
 const exportF = async () => {
-  const results = await Promise.all([listCollegeInviDetailsService(), listCollegeCountsService()])
-  const details: InviDetail[] = results[1]
-  const { exportInvisDetails } = await import('@/services/excel/Invis2Excel')
-  exportInvisDetails(results[0], details)
+  const loading = createElLoading()
+  try {
+    const results = await Promise.all([listCollegeInviDetailsService(), listCollegeCountsService()])
+    const details: InviDetail[] = results[1]
+    const { exportInvisDetails } = await import('@/services/excel/Invis2Excel')
+    exportInvisDetails(results[0], details)
+  } finally {
+    loading.close()
+  }
 }
 </script>
 <template>
