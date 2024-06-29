047d0287c13a61c1adb0b5d9ef070668d64e2c0f
BO <bwang0629@gmail.com>
2024-06-29 16:23:52 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import { createMessageDialog } from '@/components/message'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
 
 app.config.errorHandler = (err) => {
